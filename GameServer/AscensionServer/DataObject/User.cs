f7baace434606382bd55cccc330cba9922d0e176
英短 <49228668+Zt01221510@users.noreply.github.com>
2020-12-04 17:33:36 +0800

    Update:更改User表

--- GameServer/AscensionServer/DataObject/User.cs
+++ GameServer/AscensionServer/DataObject/User.cs
@@ -12,5 +12,6 @@ namespace AscensionServer
         public virtual string Account { get; set; }
         public virtual string Password { get; set; }
         public virtual string UUID { get; set; }
+        public virtual int RoleID { get; set; }
     }
 }
