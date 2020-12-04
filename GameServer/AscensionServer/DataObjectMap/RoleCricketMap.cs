e78baaf56c82c581317cb7a364308bf66afb6024
英短 <49228668+Zt01221510@users.noreply.github.com>
2020-12-04 20:52:51 +0800

    Update:角色表角色蟋蟀表映射

---
+++ GameServer/AscensionServer/DataObjectMap/RoleCricketMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+namespace AscensionServer
+{
+   public class RoleCricketMap:ClassMap<RoleCricket>
+    {
+        public RoleCricketMap()
+        {
+            Id(x => x.RoleID).GeneratedBy.Increment().Column("role_id");
+            Map(x => x.RoleCrickets).Column("role_cricket");
+            Table("role_cricket");
+        }
+    }
+}
