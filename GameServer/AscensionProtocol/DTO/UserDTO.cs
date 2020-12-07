70040a3f265827106633e1edafff0c17affb50b8
Zt01221510 <202028014@qq.com>
2020-12-07 11:15:43 +0800

    Update user
    Update user

---
+++ GameServer/AscensionProtocol/DTO/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AscensionProtocol
+{
+    [Serializable]
+    public class UserDTO
+    {
+        public virtual string Account { get; set; }
+        public virtual string Password { get; set; }
+        public virtual string UUID { get; set; }
+    }
+}
