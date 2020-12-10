b719bc91c390318c37791a135c161760285ef80f
Zt01221510 <202028014@qq.com>
2020-12-10 15:59:34 +0800

    Udpate 共有的
    Udpate 共有的

--- GameServer/AscensionServer/Command/Inventory/InventoryManager.Concreteness.cs
+++ GameServer/AscensionServer/Command/Inventory/InventoryManager.Concreteness.cs
@@ -11,80 +11,16 @@ namespace AscensionServer
     public partial class InventoryManager
     {
 
-        /// <summary>
-        /// 映射T
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="nHCriteria"></param>
-        /// <returns></returns>
-        public static T xRCriteriaSelectMethod<T>(NHCriteria nHCriteria)
-        {
-            return NHibernateQuerier.CriteriaSelect<T>(nHCriteria);
-        }
-        /// <summary>
-        ///映射 NHCriteria
-        /// </summary>
-        /// <param name="customs"></param>
-        /// <param name="values"></param>
-        /// <returns></returns>
-        public static NHCriteria xRNHCriteria(string customs, object values)
-        {
-            return GameManager.ReferencePoolManager.Spawn<NHCriteria>().SetValue(customs, values);
-        }
-        /// <summary>
-        /// 验证的映射
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="nHCriteria"></param>
-        /// <returns></returns>
-        public static bool xRVerify<T>(NHCriteria nHCriteria)
-        {
-            return  NHibernateQuerier.Verify<T>(nHCriteria);
-        }
-        /// <summary>
-        /// 统一参数
-        /// </summary>
-        /// <returns></returns>
-        public static Dictionary<byte,object> xRS2CParams()
-        {
-            Dictionary<byte, object> paramsS2CDict = new Dictionary<byte, object>();
-            return paramsS2CDict;
-        }
-        /// <summary>
-        /// 统一sub码
-        /// </summary>
-        /// <returns></returns>
-        public static Dictionary<byte, object> xRS2CSub()
-        {
-            Dictionary<byte, object> subS2CDict = new Dictionary<byte, object>();
-            return subS2CDict;
-        }
-
-        /// <summary>
-        /// 统一发送成功 
-        /// </summary>
-        /// <param name="roleId"></param>
-        /// <param name="op"></param>
-        public static void xRS2CSend(int roleId,ushort op,short rc)
-        {
-            OperationData opData = new OperationData();
-            opData.OperationCode = op;
-            opData.ReturnCode = rc;
-            opData.DataMessage = xRS2CSub();
-            GameManager.CustomeModule<RoleManager>().SendMessage(roleId, opData);
-        }
-
-
         public static void xRGetInventory(int roleId)
         {
-            var nHcriteria = xRNHCriteria("RoleID", roleId);
-            if (xRVerify<Role>(nHcriteria))
+            var nHcriteria = xRCommon.xRNHCriteria("RoleID", roleId);
+            if (xRCommon.xRVerify<Role>(nHcriteria))
             {
-                var xRserver = xRCriteriaSelectMethod<Inventory>(nHcriteria);
+                var xRserver = xRCommon.xRCriteriaSelectMethod<Inventory>(nHcriteria);
                 Utility.Debug.LogInfo("老陆==>" + xRserver.ItemDict);
-                xRS2CParams().Add((byte)ParameterCode.RoleInventory, xRserver.ItemDict);
-                xRS2CSub().Add((byte)subInventoryOp.Get, xRS2CParams());
-                xRS2CSend(roleId, (byte)ATCmd.SyncInventory,(byte)ReturnCode.Success);
+                xRCommon.xRS2CParams().Add((byte)ParameterCode.RoleInventory, xRserver.ItemDict);
+                xRCommon.xRS2CSub().Add((byte)subInventoryOp.Get, xRCommon.xRS2CParams());
+                xRCommon.xRS2CSend(roleId, (byte)ATCmd.SyncInventory,(byte)ReturnCode.Success);
             }
         }
 
---
+++ GameServer/AscensionServer/Command/Inventory/xRCommon.cs
@@ -0,0 +1,77 @@
+﻿using Cosmos;
+using Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AscensionServer
+{
+    public class xRCommon
+    {
+        /// <summary>
+        /// 映射T
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="nHCriteria"></param>
+        /// <returns></returns>
+        public static T xRCriteriaSelectMethod<T>(NHCriteria nHCriteria)
+        {
+            return NHibernateQuerier.CriteriaSelect<T>(nHCriteria);
+        }
+        /// <summary>
+        ///映射 NHCriteria
+        /// </summary>
+        /// <param name="customs"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public static NHCriteria xRNHCriteria(string customs, object values)
+        {
+            return GameManager.ReferencePoolManager.Spawn<NHCriteria>().SetValue(customs, values);
+        }
+        /// <summary>
+        /// 验证的映射
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="nHCriteria"></param>
+        /// <returns></returns>
+        public static bool xRVerify<T>(NHCriteria nHCriteria)
+        {
+            return NHibernateQuerier.Verify<T>(nHCriteria);
+        }
+        /// <summary>
+        /// 统一参数
+        /// </summary>
+        /// <returns></returns>
+        public static Dictionary<byte, object> xRS2CParams()
+        {
+            Dictionary<byte, object> paramsS2CDict = new Dictionary<byte, object>();
+            return paramsS2CDict;
+        }
+        /// <summary>
+        /// 统一sub码
+        /// </summary>
+        /// <returns></returns>
+        public static Dictionary<byte, object> xRS2CSub()
+        {
+            Dictionary<byte, object> subS2CDict = new Dictionary<byte, object>();
+            return subS2CDict;
+        }
+
+        /// <summary>
+        /// 统一发送成功 
+        /// </summary>
+        /// <param name="roleId"></param>
+        /// <param name="op"></param>
+        public static void xRS2CSend(int roleId, ushort op, short rc)
+        {
+            OperationData opData = new OperationData();
+            opData.OperationCode = op;
+            opData.ReturnCode = rc;
+            opData.DataMessage = xRS2CSub();
+            GameManager.CustomeModule<RoleManager>().SendMessage(roleId, opData);
+        }
+
+    }
+}
