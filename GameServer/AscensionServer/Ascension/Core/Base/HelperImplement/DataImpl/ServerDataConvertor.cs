e1281da8897d750d20844710db02846d24f35acf
1150463081 <63703895+1150463081@users.noreply.github.com>
2021-03-04 14:58:25 +0800

    Update:json修改

--- GameServer/AscensionServer/Ascension/Core/Base/HelperImplement/DataImpl/ServerDataConvertor.cs
+++ GameServer/AscensionServer/Ascension/Core/Base/HelperImplement/DataImpl/ServerDataConvertor.cs
@@ -52,9 +52,7 @@ namespace AscensionServer
 
                 GameManager.CustomeModule<DataManager>().TryGetValue(typeof(BattleAttackSkillData).Name, out var battleAttackSkillDataData);
                 var battleAttackSkillDataDict = TransObject<List<BattleAttackSkillData>>(battleAttackSkillDataData).ToDictionary(key => key.skillId, value => value);
-                //GameManager.CustomeModule<DataManager>().TryGetValue(typeof(BattlePassiveSkillData).Name, out var battlePassiveSkillDataData);
-
-                //var battlePassiveSkillDataDict = TransObject<List<BattlePassiveSkillData>>(battlePassiveSkillDataData).ToDictionary(key => key.skillId, value => value);
+               
                 GameManager.CustomeModule<DataManager>().TryGetValue(typeof(BattleBuffData).Name, out var battleBuffDataData);
                 var battleBuffDataDict= TransObject<List<BattleBuffData>>(battleBuffDataData).ToDictionary(key => key.buffId, value => value);
 
@@ -70,8 +68,8 @@ namespace AscensionServer
                 GameManager.CustomeModule<DataManager>().TryGetValue(typeof(MachineData).Name, out var machineData);
                 var machineDict = TransObject<List<MachineData>>(machineData).ToDictionary(key => key.RankID, value => value);
 
-                //GameManager.CustomeModule<DataManager>().TryGetValue(typeof(RankLevel).Name, out var rankLevel);
-                //var rankLevelDict = TransObject<List<RankLevel>>(rankLevel).ToDictionary(key => key.RankID, value => value);
+                bool sda= GameManager.CustomeModule<DataManager>().TryGetValue(typeof(RankLevel).Name, out var rankLevel);
+                var rankLevelDict = TransObject<List<RankLevel>>(rankLevel).ToDictionary(key => key.RankID, value => value);
                 #endregion
 
                 #region 储存方式 
@@ -89,7 +87,7 @@ namespace AscensionServer
                 GameManager.CustomeModule<DataManager>().TryAdd(CricketStatusDict);
                 GameManager.CustomeModule<DataManager>().TryAdd(battleBuffDataDict);
                 GameManager.CustomeModule<DataManager>().TryAdd(machineDict);
-                //GameManager.CustomeModule<DataManager>().TryAdd(rankLevelDict);
+                GameManager.CustomeModule<DataManager>().TryAdd(rankLevelDict);
                 #endregion
 
                 #region 获取方式
